06f9a700c0e21db8dddf2e742a1e1a0fbdf9d92e
Jordan <jordan.k.wilia@gmail.com>
2022-09-07 12:58:26 -0700

    completed everything except form. Can't figure out a problem with my input field

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -4,23 +4,52 @@ import * as actionCreators from '../state/action-creators'
 
 export function Form(props) {
 
-  const onChange = evt => {
+  const { inputChange, form, postQuiz } = props
 
-  }
+  const onChange = (evt) => {
+    console.log('id:', evt.target.id)
+    console.log('value:', evt.target.value)
+    actionCreators.inputChange({[evt.target.id]: evt.target.value});
+  };
 
   const onSubmit = evt => {
-
+    evt.preventDefault();
+    postQuiz(form);
   }
+
+  const disabled = 
+    form.newQuestion.trim() &&
+    form.newTrueAnswer.trim() &&
+    form.newFalseAnswer.trim()
 
   return (
     <form id="form" onSubmit={onSubmit}>
       <h2>Create New Quiz</h2>
-      <input maxLength={50} onChange={onChange} id="newQuestion" placeholder="Enter question" />
-      <input maxLength={50} onChange={onChange} id="newTrueAnswer" placeholder="Enter true answer" />
-      <input maxLength={50} onChange={onChange} id="newFalseAnswer" placeholder="Enter false answer" />
-      <button id="submitNewQuizBtn">Submit new quiz</button>
+      <input 
+        maxLength={50} 
+        onChange={onChange} 
+        id="newQuestion" 
+        placeholder="Enter question" 
+        value={form.newQuestion}/>
+      <input 
+        maxLength={50} 
+        onChange={onChange} 
+        id="newTrueAnswer" 
+        placeholder="Enter true answer" 
+        value={form.newTrueAnswer}/>
+      <input 
+        maxLength={50} 
+        onChange={onChange} 
+        id="newFalseAnswer" 
+        placeholder="Enter false answer" 
+        value={form.newFalseAnswer}/>
+      <button id="submitNewQuizBtn" disabled={!disabled}>Submit new quiz</button>
     </form>
   )
 }
 
-export default connect(st => st, actionCreators)(Form)
+const mapStateToProps = state => {
+  return {form: state.form}
+}
+
+export default connect(mapStateToProps, {actionCreators})(Form)
--- frontend/state/action-creators.js
+++ frontend/state/action-creators.js
@@ -10,19 +10,19 @@ export function moveCounterClockwise() {
   return{ type: MOVE_COUNTERCLOCKWISE }
  }
 
-export function selectAnswer() {
+export function selectAnswer(answerId) {
   return{ type: SET_SELECTED_ANSWER, payload: answerId }
  }
 
-export function setMessage() {
+export function setMessage(message) {
   return{ type: SET_INFO_MESSAGE, payload: message }
  }
 
-export function setQuiz() {
+export function setQuiz(quiz) {
   return{ type: SET_QUIZ_INTO_STATE, payload: quiz }
  }
 
-export function inputChange() {
+export function inputChange(input) {
   return{ type: INPUT_CHANGE, payload: input }
  }
 
@@ -33,24 +33,54 @@ export function resetForm() {
 // ❗ Async action creators
 export function fetchQuiz() {
   return function (dispatch) {
+    dispatch(setQuiz(null));
+    axios
+      .get('http://localhost:9000/api/quiz/next')
+      .then((res) => {
+        dispatch(setQuiz(res.data))
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
     // First, dispatch an action to reset the quiz state (so the "Loading next quiz..." message can display)
     // On successful GET:
     // - Dispatch an action to send the obtained quiz to its state
   }
 }
-export function postAnswer() {
+export function postAnswer(answerObj) {
   return function (dispatch) {
     // On successful POST:
     // - Dispatch an action to reset the selected answer state
     // - Dispatch an action to set the server message to state
     // - Dispatch the fetching of the next quiz
+    axios
+      .post('http://localhost:9000/api/quiz/answer', answerObj)
+      .then((res)=>{
+        dispatch(fetchQuiz())
+        dispatch(selectAnswer(null))
+        dispatch(setMessage(res.data.message))
+      })
+      .catch((err)=>console.log(err))
   }
 }
-export function postQuiz() {
+export function postQuiz({
+  question_text, 
+  true_answer_text, 
+  false_answer_text, 
+}) {
   return function (dispatch) {
-    // On successful POST:
-    // - Dispatch the correct message to the the appropriate state
-    // - Dispatch the resetting of the form
+    axios.post(`http://localhost:9000/api/quiz/new`, {
+      question_text, 
+      true_answer_text, 
+      false_answer_text, 
+    })
+    .then(res => {
+      dispatch(setMessage(`Congrats: "${res.data.question}" is a great question!`))
+      dispatch(resetForm())
+    })
+    .catch(err => {
+      console.log(err)
+    })
   }
 }
 // ❗ On promise rejections, use log statements or breakpoints, and put an appropriate error message in state
